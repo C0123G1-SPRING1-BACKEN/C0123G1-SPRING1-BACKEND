da9fe5c4879d9239382890b555e6537b51462db9
Your Name <phanvuankhang@gmail.com>
2023-07-13 16:01:32 +0700

    KhangPVA add new source code for function 6.2.4 create liquidation

--- src/main/java/com/example/back_end/dto/ContractsDto.java
+++ src/main/java/com/example/back_end/dto/ContractsDto.java
@@ -5,4 +5,46 @@ public class ContractsDto {
     private String productName;
     private Long totalCount;
     private Long loans;
+
+    public ContractsDto() {
+    }
+
+    public ContractsDto(Long id, String productName, Long totalCount, Long loans) {
+        this.id = id;
+        this.productName = productName;
+        this.totalCount = totalCount;
+        this.loans = loans;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Long getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(Long totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public Long getLoans() {
+        return loans;
+    }
+
+    public void setLoans(Long loans) {
+        this.loans = loans;
+    }
 }
