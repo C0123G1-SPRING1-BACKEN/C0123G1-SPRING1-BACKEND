3540d1634195603dfcea0f9fa91c342ea98f5687
uthoa1702 <uthoa1702@gmail.com>
2023-07-13 16:06:10 +0700

    HoaTQ add new source code of function 6.2.7

--- src/main/java/com/example/back_end/controller/RedeemingController.java
+++ src/main/java/com/example/back_end/controller/RedeemingController.java
@@ -20,13 +20,31 @@ public class RedeemingController {
     @Autowired
     private IRedeemingService iRedeemingService;
 
-
+    /**
+     * Created by: HoaTQ
+     * Date created: 13/07/2023
+     * Function: redeem the pawned object
+     *
+     * param: contractID
+     * void: update contractStatus to closed
+     */
     @Transactional
     @PatchMapping("/{id}")
     public void redeem(@PathVariable("id") Long id) {
         iRedeemingService.redeem(id);
     }
 
+
+
+    /**
+     * Created by: HoaTQ
+     * Date created: 13/07/2023
+     * Function:  getOpenContractList(page)
+     *
+     * param: page (the page number that the user want to view)
+     * return: the list of contract with the contractStatus is open
+     * issue: in progress for searching how to get list from Spring Data JPA DTO Projection
+     */
     @Transactional
     @GetMapping("/chooseContract")
     public ResponseEntity<Page<Contracts>> getOpenContractList(@RequestParam(value = "page", defaultValue = "0") Integer page) {
