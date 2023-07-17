57aedc65b857f72d9d645c10ec4e106508419b28
phanvuankhang <124018569+phanvuankhang@users.noreply.github.com>
2023-07-17 14:43:20 +0700

    Update ICustomersDto.java

--- src/main/java/com/example/back_end/dto/ICustomersDto.java
+++ src/main/java/com/example/back_end/dto/ICustomersDto.java
@@ -1,12 +1,1 @@
-package com.example.back_end.dto;
 
-
-public interface ICustomersDto {
-
-     String getId();
-     String getCustomerName();
-     String getPhoneNumber();
-     String getCitizenCode();
-     String getQuantityContract();
-
-}
