87ab381962fc9535c47bc121f5b2d0e6121509d8
uthoa1702 <uthoa1702@gmail.com>
2023-07-19 14:28:51 +0700

    ok

--- src/main/java/com/example/back_end/controller/RegisterPawnController.java
+++ src/main/java/com/example/back_end/controller/RegisterPawnController.java
@@ -15,8 +15,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.BindingType;
-import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
