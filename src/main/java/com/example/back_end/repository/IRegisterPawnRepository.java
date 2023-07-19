febc3a56626e40a3217bc72f5d6c17f17ce49274
thang516 <nguyenthangfa2001@gmail.com>
2023-07-19 10:56:30 +0700

    thangND

--- src/main/java/com/example/back_end/repository/IRegisterPawnRepository.java
+++ src/main/java/com/example/back_end/repository/IRegisterPawnRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 public interface IRegisterPawnRepository extends JpaRepository<RegisterPawn, Long> {
 
@@ -15,7 +14,7 @@ public interface IRegisterPawnRepository extends JpaRepository<RegisterPawn, Lon
    @Modifying
    @Query(value = "INSERT INTO register_pawn (name,phone,email,address,content_note,product_type_id,status) value (:name,:phone,:email,:address,:content_note,:product_type_id,false) ",nativeQuery = true)
     void createRegisterPawn(@Param("name")String name,@Param("phone")String phone,@Param("email")String email , @Param("address") String address,
-                            @Param("content_note") String contentNote,@Param("product_type_id") Long product_type_id);
+                            @Param("content_note") String contentNote,@Param("product_type_id") Long productTypeId);
 
 
    @Transactional
